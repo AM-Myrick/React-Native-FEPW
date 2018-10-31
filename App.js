bbffa728cdf12c79c01c40c940cde07a7eabb3d4
AM_Myrick <amyrick.820@gmail.com>
2018-10-31 16:42:43 -0400

    updates styling

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import axios from "axios";
-import { StyleSheet, ScrollView, TextInput, Text, Button, View } from 'react-native';
+import { StyleSheet, ScrollView, View } from 'react-native';
+import { Container, Header, Content, Form, Item, Button, Input, Label, Card, Text, CardItem, Body, Left, Right, Title } from 'native-base';
 import { createStackNavigator } from 'react-navigation';
 
 class HomeScreen extends React.Component {
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Welcome to Lambda Notes</Text>
-        <Button
-          title="See all of your notes!"
-          onPress={() => this.props.navigation.navigate('Notes')}
-        />
-        <Button
-          title="Add a new note!"
-          onPress={() => this.props.navigation.navigate("NewNote")}
-        />
+        <Text style={{ marginBottom: 20 }}>Welcome to Lambda Notes</Text>
+        <Button full onPress={() => this.props.navigation.navigate('Notes')} style={{ marginBottom: 20 }}>
+          <Text>See all of your notes!</Text>
+        </Button>
+        <Button full onPress={() => this.props.navigation.navigate("NewNote")}>
+          <Text>Add a new note!</Text>
+        </Button>
       </View>
     );
   }
@@ -41,17 +40,24 @@ class AllNotes extends React.Component {
     return (
       this.state.notes ?
       <ScrollView>
-      <View style={styles.notepage}>
-          {
+      {
               this.state.notes.map((note) => {
                   return (
-                    <View key={note._id} style={styles.notecard}>
-                      <Text style={styles.header}>{note.title}</Text>
-                      <Text>{note.textBody}</Text>
-                    </View>);
+          <Card key={note._id}>
+            <CardItem header>
+              <Text>{note.title}</Text>
+            </CardItem>
+            <CardItem>
+              <Body>
+                <Text>
+                {note.textBody}
+                </Text>
+              </Body>
+            </CardItem>
+         </Card>);
               })
           }
-      </View>
+      
       </ScrollView> : null
     );
   }
@@ -70,34 +76,63 @@ class AddNote extends React.Component {
   addANote = () => {
     axios
         .post("https://fe-notes.herokuapp.com/note/create", this.state)
-        .then(res => this.props.navigation.push('Home'))
+        .then(res => this.setState({title: "", textBody: "", _id: ""}))
+        .then(this.props.navigation.push('Home'))
         .catch(error => console.log(error));
   }
 
   render() {
     return (
-        <View>
-            <Text>Create New Note:</Text>
-            <TextInput 
-                onChangeText={(title) => this.setState({title})}
-                type="text" 
-                placeholder="Note Title"
-                name="title"
-                value={this.state.title}
-                className="new-title"
-                >
-            </TextInput>
-            <TextInput 
+        // <View>
+        //     <Text>Create New Note:</Text>
+        //     <TextInput 
+        //         onChangeText={(title) => this.setState({title})}
+        //         type="text" 
+        //         placeholder="Note Title"
+        //         name="title"
+        //         value={this.state.title}
+        //         className="new-title"
+        //         >
+        //     </TextInput>
+        //     <TextInput 
+        //         onChangeText={(textBody) => this.setState({textBody})}
+        //         type="text" 
+        //         placeholder="Note Content"
+        //         name="textBody"
+        //         value={this.state.textBody}
+        //         className="new-textBody"
+        //         >
+        //     </TextInput>
+        //     <Button title="Save" onPress={this.addANote} />
+        // </View>
+        <Container>
+        <Header>
+          <Left/>
+          <Body>
+            <Title>Create a note</Title>
+          </Body>
+          <Right />
+        </Header>
+        <Form>
+            <Item stackedLabel>
+              <Label>Note Title</Label>
+              <Input 
+              onChangeText={(title) => this.setState({title})}
+              type="text"
+              value={this.state.title}
+              />
+            </Item>
+            <Item stackedLabel last>
+              <Label>Note Text</Label>
+              <Input 
                 onChangeText={(textBody) => this.setState({textBody})}
-                type="text" 
-                placeholder="Note Content"
-                name="textBody"
+                type="text"
                 value={this.state.textBody}
-                className="new-textBody"
-                >
-            </TextInput>
-            <Button title="Save" onPress={this.addANote} />
-        </View>
+              />
+            </Item>
+            <Button full primary onPress={this.addANote}><Text>Save</Text></Button>
+          </Form>
+      </Container>
     )
   }
 }
@@ -142,47 +177,3 @@ const styles = StyleSheet.create({
     textAlign: "center",
   }
 });
-
-// import React from 'react';
-// import { Button, View, Text } from 'react-native';
-// import { createStackNavigator } from 'react-navigation';
-
-// class HomeScreen extends React.Component {
-//   render() {
-//     return (
-//       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-//         <Text>Home Screen</Text>
-//         <Button
-//           title="Go to Details"
-//           onPress={() => this.props.navigation.navigate('Details')}
-//         />
-//       </View>
-//     );
-//   }
-// }
-
-// class DetailsScreen extends React.Component {
-//   render() {
-//     return (
-//       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-//         <Text>Details Screen</Text>
-//       </View>
-//     );
-//   }
-// }
-
-// const RootStack = createStackNavigator(
-//   {
-//     Home: HomeScreen,
-//     Details: DetailsScreen,
-//   },
-//   {
-//     initialRouteName: 'Home',
-//   }
-// );
-
-// export default class App extends React.Component {
-//   render() {
-//     return <RootStack />;
-//   }
-// }
